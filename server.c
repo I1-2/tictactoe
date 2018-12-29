8664083c692082a06a2b2b56ee030627ec56d6ac
JuniorJPDJ <git@juniorjpdj.pl>
2018-12-29 18:20:06 +0100

    Please tell me it works.. Almost rewritten SELECT example code

--- server.c
+++ server.c
@@ -1,10 +1,19 @@
 #include "server.h"
+
+#define true 1
+#define false 0
+#define bool int
+
+#define BUFLEN 1024
+#define MAX_CONNECTIONS 10
+
 int main() {
-    struct Connection {
-        int active;
-        int fd;
-    };
-    int connections[10];
+    int connections[MAX_CONNECTIONS];
+    for(int i; i<MAX_CONNECTIONS; ++i){
+        connections[i] = -1;
+        // -1 is inactive connection
+    }
+
     int port = 9876;
 
     int fdListen;
@@ -14,7 +23,8 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-
+    struct sockaddr_in si_local;
+    bzero(&si_local, sizeof(struct sockaddr_in));
     si_local.sin_family = AF_INET;
     si_local.sin_port = htons(port);
     if (bind(fdListen, (const struct sockaddr *) &si_local, sizeof(si_local)) == -1) {
@@ -22,59 +32,70 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    listen(fdListen, 10);
+    listen(fdListen, MAX_CONNECTIONS);
 
+    // fd set with fds to be watched for read
     fd_set rfds;
 
-    int maxFd = fdListen;
+    char buf[BUFLEN];
 
-    FD_ZERO(&rfds);
-    FD_SET(fdListen, &rfds);
+    while(true) {
 
-    for (i = 0; i < 10; i++) {
-        if (mojePolaczenia[i].activ) {
-            maxFd = (mojePolaczenia[i].fd > maxFd) ? mojePolaczenia[i].fd : maxFd;
-            FD_SET(mojePolaczenia[i].fd, &rfds);
-        }
-    }
-// WYWOLANIE FUNKCJI SELECT
+        int maxFd = fdListen;
 
-/* Czekanie nie dłużej niż sekund. */
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-    retval = select(maxFd + 1, &rfds, NULL, NULL, &tv);
+        FD_ZERO(&rfds);
+        FD_SET(fdListen, &rfds);
 
-    if (FD_ISSET(fdListen, &rfds)) {
-        int polIdx = 0;
-        for (polIdx = 0; polIdx < 10; polIdx++) {
-            if (mojePolaczenia[polIdx].activ == 0) { //Przyjmowanie nowego połaczenia
-                mojePolaczenia[polIdx].activ = 1;
-                mojePolaczenia[polIdx].fd = accept(fdListen, NULL, NULL);
-                printf("Nowe polaczenie\n");
-                break;
+        for (int i = 0; i < MAX_CONNECTIONS; ++i) {
+            if (connections[i] != -1) {
+                maxFd = (connections[i] > maxFd) ? connections[i] : maxFd;
+                FD_SET(connections[i], &rfds);
             }
         }
-        if (polIdx == 10) {  //Serwer obsługuje 10 połączeń, odrzucanie połączenia
-            int tmpFd = accept(fdListen, NULL, NULL);
-            send(tmpFd, "Odmowa\n", 7, 0);
-            close(tmpFd);
+// WYWOLANIE FUNKCJI SELECT
+
+        struct timeval tv;
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        int retval = select(maxFd + 1, &rfds, NULL, NULL, &tv);
+        // select obserwujacy rfds i czekajacy max 5 sekund
+
+        if (FD_ISSET(fdListen, &rfds)) {
+            int i = 0;
+            for (i = 0; i < MAX_CONNECTIONS; ++i) {
+                if (connections[i] == -1) {
+                    // przyjmowanie nowego połaczenia
+                    connections[i] = accept(fdListen, NULL, NULL);
+                    printf("Nowe polaczenie\n");
+                    break;
+                }
+            }
+            if (i == MAX_CONNECTIONS) {
+                // zamkniecie nowego polaczenia, nie ma juz dla niego miejsca
+                int tmpFd = accept(fdListen, NULL, NULL);
+                close(tmpFd);
+            }
         }
-    }
-    if (FD_ISSET(mojePolaczenia[i].fd, &rfds)) {
-        memset(buf, 0, sizeof(char) * BUFLEN);
-        int odczytano = recv(mojePolaczenia[i].fd, buf, BUFLEN, 0);
-        if (odczytano < 1) { //Zakończenie połączenia
-            mojePolaczenia[i].activ = 0;
-            close(mojePolaczenia[i].fd);
-            mojePolaczenia[i].fd = -1;
-        } else {
-            printf("Odebrałem od klienta %d: %s", i, buf);
-            int polIdx;
-            for (polIdx = 0; polIdx < 10; polIdx++) { //Rozsyłanie wiadomości do pozostałych połączeń
-                if (polIdx == i)
-                    continue;
-                if (mojePolaczenia[polIdx].activ)
-                    send(mojePolaczenia[polIdx].fd, buf, odczytano, 0);
+
+        for(int i = 0; i < MAX_CONNECTIONS; ++i) {
+            if (FD_ISSET(connections[i], &rfds)) {
+                memset(buf, 0, sizeof(char) * BUFLEN);
+                int received = recv(connections[i], buf, BUFLEN, 0);
+                if (received < 1) {
+                    // zakończenie połączenia
+                    close(connections[i]);
+                    connections[i] = -1;
+                } else {
+                    printf("Odebrałem od klienta %d: %s", i, buf);
+                    int j;
+                    for (j = 0; j < MAX_CONNECTIONS; ++j) {
+                        // rozsyłanie wiadomości do pozostałych połączeń
+                        if (j == i)
+                            continue;
+                        if (connections[j] != -1)
+                            send(connections[j], buf, received, 0);
+                    }
+                }
             }
         }
 
--- server.h
+++ server.h
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <errno.h>
